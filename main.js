7fba8565c95d4b791aed2676e869d427a0ff2554
IDAbyLIGL <haakonu97@gmail.com>
2019-02-27 22:53:49 +0100

    Doing some more prepping for functionality
    Still just making stuff ready and setting up structure

--- main.js
+++ main.js
@@ -1,71 +1,88 @@
-const http = require('http');
-const url = require('url');
-const fs = require('fs');
-const path = require('path');
-const opn = require('opn');
+const http = require("http");
+const url = require("url");
+const fs = require("fs");
+const path = require("path");
+const opn = require("opn");
 
-http.createServer(function (req, res) {
-  // console.log(`${req.method} ${req.url}`);
+http
+  .createServer(function(req, res) {
+    // console.log(`${req.method} ${req.url}`);
 
-  // parse URL
-  const parsedUrl = url.parse(req.url);
-  // extract URL path
-  let pathname = `.${parsedUrl.pathname}`;
-  // based on the URL path, extract the file extention. e.g. .js, .doc, ...
-  const ext = path.parse(pathname).ext;
-  // maps file extention to MIME typere
-  const map = {
-    '.ico': 'image/x-icon',
-    '.html': 'text/html',
-    '.js': 'text/javascript',
-    '.json': 'application/json',
-    '.css': 'text/css',
-    '.png': 'image/png',
-    '.jpg': 'image/jpeg',
-    '.wav': 'audio/wav',
-    '.mp3': 'audio/mpeg',
-    '.svg': 'image/svg+xml',
-    '.pdf': 'application/pdf',
-    '.doc': 'application/msword'
-  };
-
-  let tryDownload = (path) => { // this is now our download hook
-    console.log(`Try downloading ${pathname}`)
-    return 404 // res.statusCode
-  }
-  fs.exists(pathname, function (exist) {
-    if(!exist) {
-      if (pathname.startsWith("./download/")){ // Check if it's trying to download
-        res.statusCode = tryDownload(pathname)
-        res.statusCode == 404 ? res.end(`File ${pathname} not found!`) : res.end()
-      }else{
-        // if the file is not found, return 404
-        res.statusCode = 404;
-        res.end(`File ${pathname} not found!`);
-      }
-      return;
-    }
-
-    // if is a directory search for index file matching the extention
-    if (fs.statSync(pathname).isDirectory()){
-        pathname += 'index.html'
+    // parse URL
+    const parsedUrl = url.parse(req.url);
+    // extract URL path
+    let pathname = `.${parsedUrl.pathname}`;
+    // based on the URL path, extract the file extention. e.g. .js, .doc, ...
+    const ext = path.parse(pathname).ext;
+    // maps file extention to MIME typere
+    const map = {
+      ".ico": "image/x-icon",
+      ".html": "text/html",
+      ".js": "text/javascript",
+      ".json": "application/json",
+      ".css": "text/css",
+      ".png": "image/png",
+      ".jpg": "image/jpeg",
+      ".wav": "audio/wav",
+      ".mp3": "audio/mpeg",
+      ".svg": "image/svg+xml",
+      ".pdf": "application/pdf",
+      ".doc": "application/msword"
     };
+    let fixCase = text => {
+      return decodeURI(
+        text
+          .replace(/(?:^\w|[A-Z]|\b\w)/g, function(letter, index) {
+            return index == 0 ? letter.toLowerCase() : letter.toUpperCase();
+          })
+          .replace(/\s+/g, "")
+      );
+    };
+    let tryDownload = path => {
+      // this is now our download hook
+      // example url: ./download/here is the filename|1|1
+      let data = pathname.substring(11).split("%7C");
+      let title = fixCase(data[0]);
+      let type = data[1];
+      console.log(`Try downloading ${title} | ${type}`);
 
-    // read file from file system
-    fs.readFile(pathname, function(err, data){
-      if(err){
-        res.statusCode = 500;
-        res.end(`Error getting the file: ${err}.`);
-      } else {
-        // if the file is found, set Content-type and send data
-        res.setHeader('Content-type', map[ext] || 'text/html' );
-        res.end(data);
+      return 404; // res.statusCode
+    };
+    fs.exists(pathname, function(exist) {
+      if (!exist) {
+        if (pathname.startsWith("./download/")) {
+          // Check if it's trying to download
+          res.statusCode = tryDownload(pathname);
+          res.statusCode == 404
+            ? res.end(`File ${pathname} not found!`)
+            : res.end();
+        } else {
+          // if the file is not found, return 404
+          res.statusCode = 404;
+          res.end(`File ${pathname} not found!`);
+        }
+        return;
       }
+
+      // if is a directory search for index file matching the extention
+      if (fs.statSync(pathname).isDirectory()) {
+        pathname += "index.html";
+      }
+
+      // read file from file system
+      fs.readFile(pathname, function(err, data) {
+        if (err) {
+          res.statusCode = 500;
+          res.end(`Error getting the file: ${err}.`);
+        } else {
+          // if the file is found, set Content-type and send data
+          res.setHeader("Content-type", map[ext] || "text/html");
+          res.end(data);
+        }
+      });
     });
-  });
-
-
-}).listen(8080);
+  })
+  .listen(8080);
 
 console.log(`Server listening on port 8080`);
-opn("http://localhost:" + 8080)
+opn("http://localhost:" + 8080);
--- renderer.js
+++ renderer.js
@@ -1,62 +1,94 @@
-let title = document.querySelector(".title")
-let form = document.querySelector(".form")
-let button = document.querySelector("button")
-let type = document.querySelector(".type")
-let prettySetup = document.querySelector(".prettySetup")
-let exportOptions = document.querySelector(".export")
-let loader = document.querySelector(".loader")
-let h1 = document.querySelector("h1")
+let title = document.querySelector(".title");
+let form = document.querySelector(".form");
+let button = document.querySelector("button");
+let type = document.querySelector(".type");
+let prettySetup = document.querySelector(".prettySetup");
+let exportOptions = document.querySelector(".export");
+let loader = document.querySelector(".loader");
+let h1 = document.querySelector("h1");
 let globalState = {
   type: "web",
   title: "",
   export: "disk"
-}
+};
 window.onload = () => {
-    type.focus();
-}
+  type.focus();
+  type.value == "nodejs"
+    ? changeState("nodejs", "type")
+    : changeState("web", "type");
+  globalState.export = exportOptions.value;
+};
 button.addEventListener("click", () => {
   button.blur();
-  loader.classList.add("show")
-  form.classList.add("hide")
-  h1.classList.add("hide")
+  loader.classList.add("show");
+  form.classList.add("hide");
+  h1.classList.add("hide");
   // build
-  setTimeout(() =>{
-  loader.classList.remove("show")
-  form.classList.remove("hide")
-  h1.classList.remove("hide")
-  }, 2500)
-})
-title.addEventListener("keypress", (e) => {
+  if (globalState.export == "disk") {
+    exportToDisk();
+  } else if (globalState.export == "github") {
+    exportToGitHub();
+  } else if (globalState.export == "codepen") {
+    exportToCodePen();
+  }
+});
+let exportToDisk = () => {
+  let downloadLink = "/download/" + globalState.title + "|" + globalState.type;
+  window.location.href = downloadLink;
+};
+let exportToGitHub = () => {
+  // Export to GitHub
+  setTimeout(() => {
+    loader.classList.remove("show");
+    form.classList.remove("hide");
+    h1.classList.remove("hide");
+  }, 2500);
+};
+let exportToCodePen = () => {
+  // Export to CodePen
+  setTimeout(() => {
+    loader.classList.remove("show");
+    form.classList.remove("hide");
+    h1.classList.remove("hide");
+  }, 2500);
+};
+title.addEventListener("keyup", e => {
   let key = e.which || e.keyCode;
   if (key === 13) {
-    exportOptions.focus()
+    exportOptions.focus();
   }
-})
-type.addEventListener("change", ()=>{
-  type.value == "nodejs" ? changeState("nodejs", "type") : changeState("web", "type")
-  title.focus()
-})
-exportOptions.addEventListener("change", ()=>{button.focus()})
+  globalState.title = title.value;
+});
+type.addEventListener("change", () => {
+  type.value == "nodejs"
+    ? changeState("nodejs", "type")
+    : changeState("web", "type");
+  title.focus();
+});
+exportOptions.addEventListener("change", () => {
+  button.focus();
+  globalState.export = exportOptions.value;
+});
 let changeState = (state, method) => {
-  if (method == "type"){
-    switch (state){
+  if (method == "type") {
+    switch (state) {
       case "nodejs":
         exportOptions.innerHTML = `
           <option value="disk">Disk</option>
           <option value="github">GitHub</option>
-        `
-        globalState.type = "nodejs"
-      break;
+        `;
+        globalState.type = "nodejs";
+        break;
       case "web":
       default:
         exportOptions.innerHTML = `
             <option value="disk">Disk</option>
             <option value="codepen" class="notnode">CodePen</option>
             <option value="github">GitHub</option>
-        `
-        globalState.type = "web"
-      break;
+        `;
+        globalState.type = "web";
+        break;
     }
   }
-  return globalState
-}
+  return globalState;
+};
